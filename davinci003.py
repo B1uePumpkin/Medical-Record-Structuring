d5439d8c7ce2c3b7de71151b35fb1a4fdc6c876b
yuhaodai <93068257+B1uePumpkin@users.noreply.github.com>
2023-11-09 03:52:38 +0800

    davinci003 prompt fixed

--- davinci003.py
+++ davinci003.py
@@ -12,7 +12,7 @@ openai.api_key = dotenv_values('.env')["API_KEY"]
 # b)expecting format
 # c)example
 prompt = """
-我希望將以下資料抽取部分欄位轉換成有主次關係的表格呈現結構化資料
+我希望將以下資料抽取部分欄位轉換成有主次關係的表格呈現結構化資料，並且除了專有名詞外都用中文表示
 ===
 SNOMED:59000-A-81403.
 
@@ -91,3 +91,28 @@ res = openai.Completion.create(
 
 # print respond
 print (res["choices"][0]["text"])
+
+# 輸出結果
+# | 欄位 | 資料 |
+# | --- | --- |
+# | 診斷資料號 | 59000-A-81403 |
+# | 診斷結果 | Bloody stool   |
+# | **檢體觀察** |  |
+# | 組織片數 | 5片 |
+# | 組織尺寸 | 0.1*0.1*0.1 cm |
+# | 組織外觀 | 灰白、彈性 |
+# | **醫學檢驗**   |  |
+# | 病灶區 | 腸管，上行腸  |
+# | 病理判讀 | 由非常細胞形成的腺癌 |
+# | 病解專醫字第 | 000477號 |
+# | 前設情況 | Remnants 示範Adenoma |
+# | 附註 |  |
+# | **其他檢查** |  |
+# | 診斷資料號 | S04-05069 |
+# | 診斷結果 | Gall bladder, cholecystectomy |
+# | 病理判讀 | Acute gangrenous cholecystitis |
+# | 診斷資料號 | S01-01737 |
+# | 診斷結果 | Skin, nasal bridge, excisional biopsy |
+# | 病理判讀 | Basal cell carcinoma |
+# | 住院醫師 |  |
+# | 病理醫師 | Shu-Han Huang, M.D./SWH |

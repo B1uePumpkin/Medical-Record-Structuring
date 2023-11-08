cb7dc9228e7682b2732b52074c45a02e9b113502
andylin9611052 <andylin9611052@gmail.com>
2023-11-09 06:11:27 +0800

    search

--- app.py
+++ app.py
@@ -2,70 +2,16 @@
 
 import openai
 from dotenv import dotenv_values
-from flask import Flask
+from flask import Flask, request
+
 openai.api_key = dotenv_values(".env")['API_KEY']
 app = Flask(__name__)
-prompt = """
-我希望將以下資料抽取部分欄位轉換成有主次關係的表格呈現結構化資料,除了專有名詞外請都用繁體中文顯示,內容必須完整的顯示
-===
-SNOMED:59000-A-81403. 
 
-CLINICAL HISTORY AND DIAGNOSIS:
-
-Bloody stool
-
-DIAGNOSIS:   
-
-Intestine, large, labeled as "Ascending colon", endoscopic biopsy
-
-                                                      --- Adenocarcinoma
-
-GROSS DESCRIPTION:  
-
-The specimen submitted consists of 5 tissue fragments measuring up
-
-to 0.5 x 0.2 x 0.2 cm in size, fixed in formalin. They are gray
-
-white and elastic.
-
-All for section is taken.   
-
-MICROSCOPIC DESCRIPTION:
-
-Section shows fragments of necrotic debris and colon mucosa with
-
-proliferation and infiltration of irregular hyperchromatic neoplas-
-
-tic glands arranged mainly in complicated tubulo-papillary fashion,  
-
-a moderately differentiated adenocarcinoma. Remnants suggestive of a
-
-pre-existing adenoma are not seen.
-
-REFERENCE:   
-
-S04-05069
-
-Gall bladder, cholecystectomy
-
-                                --- Acute gangrenous cholecystitis  
-
-S01-01737   
-
-Skin, nasal bridge, excisional biopsy --- Basal cell carcinoma
-
-                                   PAGE 1/1
-
-                            --- END OF REPORT ---
-
-
-住院醫師:                   病理醫師: Shu-Han Huang, M.D./SWH
-
-                                               病解專醫字第000477號  
-===
-
-輸出範例:  
-
+# 預先定義的固定prompt
+fixed_prompt = """
+請先完整查看輸入內容
+根據以下結構產生完整表格，此表格只是架構範例，內容請看輸入病例:
+輸出範例(請根據使用者輸入內容做判斷，勿直接填入範例內容):
 ===
 
 | 欄位 | 資料 |  
@@ -82,35 +28,45 @@ Skin, nasal bridge, excisional biopsy --- Basal cell carcinoma
 
 | 組織外觀 | 灰 |
 
-...以此類推  
-
+...以此類推
 ===
+除了專有名詞，其餘請以繁體中文顯示，內容須正確完整並對齊
 """
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    # 生成結果
-    res = openai.Completion.create(model="text-davinci-003", prompt=prompt, max_tokens=1000)
-    text = res["choices"][0]["text"]
+    if request.method == "POST":
+        # 獲取表單中的用戶輸入prompt
+        user_input = request.form["prompt"]
+        # 將用戶輸入的prompt和預先定義的固定prompt結合
+        combined_prompt = fixed_prompt + user_input
+        # 生成結果
+        res = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=combined_prompt,
+            max_tokens=1000
+        )
+        text = res["choices"][0]["text"]
 
-    # 轉換為HTML
-    html = "<html><body><table>"
-    lines = text.split("\n")
-    for line in lines:
-        if "|" in line:
-            # 移除空格並分割成欄位
-            columns = [col.strip() for col in line.split("|")]
-            # 構建HTML表格行
-            html += "<tr>"
-            for col in columns:
-                html += f"<td>{col}</td>"
-            html += "</tr>"
-    html += "</table></body></html>"
+        # 轉換為HTML
+        html = "<html><body><table>"
+        lines = text.split("\n")
+        for line in lines:
+            if "|" in line:
+                # 移除空格並分割成欄位
+                columns = [col.strip() for col in line.split("|")]
+                # 構建HTML表格行
+                html += "<tr>"
+                for col in columns:
+                    html += f"<td>{col}</td>"
+                html += "</tr>"
+        html += "</table></body></html>"
 
-    # 直接返回HTML內容
-    return html
+        # 直接返回HTML內容
+        return html
 
-
+    # 顯示輸入表單
+    return "<html><body><form method='post'><label for='prompt'>請輸入病例：</label><input type='text' name='prompt' size='100'><input type='submit' value='提交'></form></body></html>"
 
 if __name__ == "__main__":
     app.run()

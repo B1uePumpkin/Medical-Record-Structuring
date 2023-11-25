41d4d533bc691c5c52c6d647b0790e5039cf7bbc
andylin9611052 <andylin9611052@gmail.com>
2023-11-25 21:27:25 +0800

    前端分離

--- app.py
+++ app.py
@@ -2,14 +2,15 @@
 
 import openai
 from dotenv import dotenv_values
-from flask import Flask, request
+from flask import Flask, render_template, request
 
 openai.api_key = dotenv_values(".env")['API_KEY']
-app = Flask(__name__)
+app = Flask(__name__, template_folder='frontend')
+app.static_folder = 'frontend'
 
-# 預先定義的固定prompt
+# 預先定義的固定 prompt
 fixed_prompt = """
-首先完整查看使用者輸入的病例內容，再來根據這些信息產生一份完整的病例表格，格式內容請參考下面輸出範例，請確保欄位籍資料內容須正確相符、完整並對齊，最後檢查內容無誤再以繁體中文輸出，專有名詞以及姓名除外。
+首先完整查看使用者輸入的病例內容，再來根據這些信息產生一份完整的病例表格，格式內容請參考下面輸出範例，請確保欄位籍資料內容須正確相符、完整且對齊，表格結構需要正確，最後檢查內容無誤再以繁體中文輸出，專有名詞以及姓名除外。
 以下是輸出範例格式:
 ===
 
@@ -31,13 +32,12 @@ fixed_prompt = """
 ===
 """
 
-
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        # 獲取表單中的用戶輸入prompt
+        # 獲取表單中的用戶輸入 prompt
         user_input = request.form["prompt"]
-        # 將用戶輸入的prompt和預先定義的固定prompt結合
+        # 將用戶輸入的 prompt 和預先定義的固定 prompt 結合
         combined_prompt = fixed_prompt + user_input
         # 生成結果
         res = openai.Completion.create(
@@ -47,25 +47,14 @@ def index():
         )
         text = res["choices"][0]["text"]
 
-        # 轉換為HTML
-        html = "<html><body><table>"
+        # 將 text 轉換為 HTML
         lines = text.split("\n")
-        for line in lines:
-            if "|" in line:
-                # 移除空格並分割成欄位
-                columns = [col.strip() for col in line.split("|")]
-                # 構建HTML表格行
-                html += "<tr>"
-                for col in columns:
-                    html += f"<td>{col}</td>"
-                html += "</tr>"
-        html += "</table></body></html>"
 
-        # 直接返回HTML內容
-        return html
+        # 渲染模板並返回 HTML
+        return render_template("index.html", lines=lines)
 
     # 顯示輸入表單
-    return "<html><body><form method='post'><label for='prompt'>請輸入病例：</label><br/> <textarea name='prompt' rows='50' cols='100'></textarea><input type='submit' value='提交'></form></body></html>"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
